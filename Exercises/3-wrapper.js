cb162f3c0e3130ec347cedead15dd44c86e48550
GrigoriyUstymenko <gromgoriy@gmail.com>
2020-11-27 00:06:56 +0200

    Tasks

--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -1,5 +1,23 @@
 'use strict';
 
-const contract = (fn, ...types) => null;
+
+const contract = (fn, ...types) => (...args) => {
+  let sameTypes = true;
+  for (let i = 0; i < args.length; i++) {
+    const def = types[i];
+    const argument = args[i];
+    sameTypes = typeof argument === def.name.toLowerCase();
+    if (!sameTypes) {
+      throw new TypeError('This is an error, deal with it');
+    }
+  }
+  const last = types.length - 1;
+  const res = fn(...args);
+  sameTypes = typeof res === types[last].name.toLowerCase();
+  if (!sameTypes) {
+    throw new TypeError('This is an error, deal with it');
+  }
+  return res;
+};
 
 module.exports = { contract };
